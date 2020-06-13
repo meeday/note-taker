8a01c8151184426c426b21f56ad815bf06d66bf5
meeday <59993824+meeday@users.noreply.github.com>
2020-06-13 19:01:29 +0100

    dirname edited

--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3300;
 
 app.use(express.urlencoded({ extened: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.static('./'));
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(_dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(_dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
 app.get("/api/notes", (req, res) => {
